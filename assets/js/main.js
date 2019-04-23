f8b4e92f794ee305013bb0fe61511ad2008b2baf
core.editor <andreeavillatte@gmail.com>
2019-04-23 15:18:51 +0200

    une autre possibilité

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,6 +3,11 @@ $(document).ready(function () {
     //si je double click
     $('#image').dblclick(function () {
         //je peux modifier la largeur de l'image qui a l'ID image
-        $('#image').css('width', '450');
+        // $('#image').css('width', '450');
+        // 'this' on l'a utilisé pour faire référence à l'élément déclancheur de la fonction
+        // cette metode ou la suivante
+        // $(this).css('width', '450');
+        // suivante
+        $(this).width(500) // on mets pas d'unités de mésure
     });
 });
