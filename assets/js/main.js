2c49bfa72d54171858c55606b5f04af1acd329c9
core.editor <andreeavillatte@gmail.com>
2019-04-23 15:11:17 +0200

    après corrections

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,7 @@
 //quand la page est chargée (ready)
 $(document).ready(function () {
     //si je double click
-    $('#clickMe').dblclick(function () {
+    $('#image').dblclick(function () {
         //je peux modifier la largeur de l'image qui a l'ID image
         $('#image').css('width', '450');
     });
